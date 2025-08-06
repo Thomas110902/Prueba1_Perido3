4fd175d6423e8cc669a6e1ba35530e9d24e88535
Thomas <teluis@scl.edu.gt>
2025-08-06 07:15:07 -0600

    Login

---
+++ pages/login/login.js
@@ -0,0 +1,9 @@
+export function login(){
+    let div = document.createElement('div');
+    div.className = "div_banner2";
+    div.textContent = "Hola";
+
+
+    return div;
+}
+document.body.appendChild(login());
