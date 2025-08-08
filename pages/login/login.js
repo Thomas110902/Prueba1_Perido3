d597550a51334309f209e23aa0f770aa0edb81ec
Thomas <teluis@scl.edu.gt>
2025-08-07 18:26:58 -0600

    Login2.3

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,9 +1,48 @@
-export function login(){
+export function login() {
     let div = document.createElement('div');
     div.className = "div_banner2";
-    div.textContent = "Hola";
 
+    // Título
+    let h2 = document.createElement('h2');
+    h2.textContent = "Geeta.";
+    h2.className = "titulo";
+    div.appendChild(h2);
+
+    // Párrafo 1 en negrita
+    let p1 = document.createElement('p');
+    p1.className = "p1";
+    p1.innerHTML = "Create your fashion in your own way";
+    div.appendChild(p1);
+
+    // Párrafo 2 normal
+    let p2 = document.createElement('p');
+    p2.className = "p2";
+    p2.textContent = "Each men and women has their own style, Geeta help you to create your unique style. ";
+    div.appendChild(p2);
+
+    // Botón de Login
+    let loginBtn = document.createElement('button');
+    loginBtn.textContent = "Login";
+    loginBtn.className = "btn-login";
+    loginBtn.addEventListener('click',()=> { alert ("Cuidadoo")
+        div.classList.add("ocultar");
+    });
+    div.appendChild(loginBtn);
+
+    // Span con el texto OR en negrita 
+    let spanOr = document.createElement('span');
+    spanOr.innerHTML = "OR";
+    spanOr.className = "span-or";
+    div.appendChild(spanOr);
+
+    // Botón Register
+    let registerBtn = document.createElement('button');
+    registerBtn.textContent = "Register >";
+    registerBtn.className = "btn-register";
+    div.appendChild(registerBtn);
 
     return div;
 }
+
+// Agrega al body
 document.body.appendChild(login());
